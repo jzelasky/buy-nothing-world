57976a3d636ccf386eac9a6e031640f346a071d7
Jessica Zelasky <jzelasky@gmail.com>
2023-05-03 16:05:54 -0400

    styled footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import icon from './img/globe-icon.png'
 
 export default function Header() {
   return (
-    <div>
-      Footer
+    <div className='footerStyles d-flex justify-content-center'>
+      <img src={icon} alt="globe icon" className='p-5'></img>
     </div>
   );
 }
