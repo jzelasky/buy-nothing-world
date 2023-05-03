0b4c64a04ac8aa0b262a7f69ef0d1c90cfa126bf
Jessica Zelasky <jzelasky@gmail.com>
2023-05-03 15:37:17 -0400

    styled header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div>
-      Header
+      <h1 className='p-5 headerStyles'>Buy Nothing World</h1>
     </div>
   );
 }
