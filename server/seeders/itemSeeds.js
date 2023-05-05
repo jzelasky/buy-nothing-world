bae23b181bd25a50166ccbaea5b5552abd7319bf
Jessica Zelasky <jzelasky@gmail.com>
2023-05-05 10:39:25 -0400

    added seed data

---
+++ server/seeders/itemSeeds.js
@@ -0,0 +1,23 @@
+[
+    {
+      "itemText": "Lorem ipsum dolor sit amet. Quo exercitationem accusamus est necessitatibus dolore aut nesciunt tenetur id aspernatur quod et omnis voluptatem et consequuntur aperiam. Qui itaque consequatur sed quibusdam laboriosam et perferendis voluptates ex repudiandae illo id enim mollitia et voluptas sequi.",
+      "itemAuthor": "Brian Kernighan"
+    },
+    {
+      "itemText": "Lorem ipsum dolor sit amet. Quo exercitationem accusamus est necessitatibus dolore aut nesciunt tenetur id aspernatur quod et omnis voluptatem et consequuntur aperiam. Qui itaque consequatur sed quibusdam laboriosam et perferendis voluptates ex repudiandae illo id enim mollitia et voluptas sequi.",
+      "itemAuthor": "Max Kanat-Alexander"
+    },
+    {
+      "itemText": "Lorem ipsum dolor sit amet. Quo exercitationem accusamus est necessitatibus dolore aut nesciunt tenetur id aspernatur quod et omnis voluptatem et consequuntur aperiam. Qui itaque consequatur sed quibusdam laboriosam et perferendis voluptates ex repudiandae illo id enim mollitia et voluptas sequi.",
+      "itemAuthor": "Why The Lucky Stiff"
+    },
+    {
+      "itemText": "Lorem ipsum dolor sit amet. Quo exercitationem accusamus est necessitatibus dolore aut nesciunt tenetur id aspernatur quod et omnis voluptatem et consequuntur aperiam. Qui itaque consequatur sed quibusdam laboriosam et perferendis voluptates ex repudiandae illo id enim mollitia et voluptas sequi.",
+      "itemAuthor": "Kent Beck"
+    },
+    {
+      "itemText": "Lorem ipsum dolor sit amet. Quo exercitationem accusamus est necessitatibus dolore aut nesciunt tenetur id aspernatur quod et omnis voluptatem et consequuntur aperiam. Qui itaque consequatur sed quibusdam laboriosam et perferendis voluptates ex repudiandae illo id enim mollitia et voluptas sequi.",
+      "itemAuthor": "Edward V. Berard"
+    },
+  ]
+  
