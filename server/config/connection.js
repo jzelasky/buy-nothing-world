2c816cedb8f6a0e5af100c9d0b2c01fd8fecae15
Jessica Zelasky <jzelasky@gmail.com>
2023-05-11 18:22:57 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose' || 'mongodb://127.0.0.1:27017/buy-nothing-world');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
 
